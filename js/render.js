6da4d0de1975b61473526ba124cc44635cd4dbf0
nestorhdez <nestorhdez.26@gmail.com>
2018-09-28 18:59:12 +0100

    Putting the request of the json info into a function to make it reusable

--- js/render.js
+++ js/render.js
@@ -1,38 +1,58 @@
-//Request the JSON document
-req=new XMLHttpRequest();
-req.open("GET",'https://jsonplaceholder.typicode.com/users',true);
-req.send();
-req.onload=function(){
-    //Parse make the data become to an JS object
-    var json = JSON.parse(req.responseText);
+var userJson;
+
+function getAjaxJsonUsers( callback ){
+
+    var json;
+    //Request the JSON document
+    req=new XMLHttpRequest();
+    req.open("GET",'https://jsonplaceholder.typicode.com/users',true);
+    req.addEventListener("load", callback, false);
+    req.send();
+
+    return json;
+};
 
     // Create the structure of the card with the json data
+var createCard =  function (e) {
 
-    json.forEach(function(val) {
+    //Change the json info to a js object
+    var json = JSON.parse(e.target.responseText);
 
-        //Create a card
-        let card = (`
+        // Create the structure of the card with the json data per user
+        json.forEach(function(val) {
 
-            <div class="card mr-3 mb-3 card-user" style="width: 18rem;">
-                <div class="card-body text-center">
-                    <div class="mb-3">
-                        <i class="fas fa-user" style="font-size: 2rem;"></i> <br>
-                        <h5 class="card-title d-inline user-name">${val.name}</h5>
+            //Create a card
+            let card = (`
+
+                <div class="card mr-3 mb-3 card-user" style="width: 18rem;">
+                    <div class="card-body text-center">
+                        <div class="mb-3">
+                            <i class="fas fa-user mb-3" style="font-size: 2rem;"></i> <br>
+                            <h5 class="card-title d-inline user-name">${val.name}</h5>
+                        </div>
+                        <div class="text-left">
+                            <h6 class="card-subtitle mb-3">Contact information</h6>
+                            <p class="card-text font-weight-bold">
+                                City: <span class="font-weight-normal">${val.address.city}</span><br>
+                                Email: <a href="mailto:${val.email}">${val.email}</a><br>
+                                Phone: <span class="font-weight-normal">${val.phone}</span><br>
+                            </p>
+
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Card link</a>
+                            
+                        </div>
                     </div>
-                    <h6 class="card-subtitle mb-3 text-muted">City: ${val.address.city}</h6>
-                    <p class="card-text">
-                        Phone: ${val.phone}
-                    </p>
                 </div>
-            </div>
-        `);
+            `);
 
-         //Add the user tu a card
-        document.getElementById('card-container').innerHTML += card;
+            //Add the card to the html
+            document.getElementById('card-container').innerHTML += card;
 
-    });
+        });
+    };
 
-};
+    getAjaxJsonUsers(createCard);
 
 //Filter data with the form
 
@@ -41,8 +61,8 @@ let list = document.querySelector('#card-container');
 
 let searchBar = document.querySelector('#adv-search-form');
 
-console.log(list);
-console.log(searchBar);
+// console.log(list);
+// console.log(searchBar);
 
 //Make the filter when submit the form
 searchBar.addEventListener('submit', function(e){
@@ -54,8 +74,8 @@ searchBar.addEventListener('submit', function(e){
     
     let users = list.getElementsByClassName('card-user');
 
-    console.log(e);
-    console.log(users);
+//     console.log(e);
+//     console.log(users);
 
     //Create a loop through the cards
     Array.from(users).forEach(function(user){
