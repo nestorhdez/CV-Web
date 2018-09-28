2bda94682ddc71010797896ef70c419a946c0f82
nestorhdez <nestorhdez.26@gmail.com>
2018-09-28 16:38:19 +0100

    Filter users by name with the input-name of the form

--- js/render.js
+++ js/render.js
@@ -13,11 +13,11 @@ req.onload=function(){
         //Create a card
         let card = (`
 
-            <div class="card mr-3 mb-3" style="width: 18rem;">
+            <div class="card mr-3 mb-3 card-user" style="width: 18rem;">
                 <div class="card-body text-center">
                     <div class="mb-3">
                         <i class="fas fa-user" style="font-size: 2rem;"></i> <br>
-                        <h5 class="card-title d-inline">${val.name}</h5>
+                        <h5 class="card-title d-inline user-name">${val.name}</h5>
                     </div>
                     <h6 class="card-subtitle mb-3 text-muted">City: ${val.address.city}</h6>
                     <p class="card-text">
@@ -33,3 +33,43 @@ req.onload=function(){
     });
 
 };
+
+//Filter data with the form
+
+
+let list = document.querySelector('#card-container');
+
+let searchBar = document.querySelector('#adv-search-form');
+
+console.log(list);
+console.log(searchBar);
+
+//Make the filter when submit the form
+searchBar.addEventListener('submit', function(e){
+    //Don't refresh the page when submit
+    e.preventDefault();
+    
+    //Put the value of the input name in lower case into a variable
+    let term = document.querySelector('#input-name').value.toLowerCase();
+    
+    let users = list.getElementsByClassName('card-user');
+
+    console.log(e);
+    console.log(users);
+
+    //Create a loop through the cards
+    Array.from(users).forEach(function(user){
+        
+        //Put every user name into a var
+        let title = user.querySelector('.user-name').textContent;
+            console.log(title);
+            
+        //If the user has some character equal to the search of the
+        //input it will be display block
+        if (title.toLowerCase().indexOf(term) != -1){
+            user.style.display = 'block';
+            } else {
+                user.style.display = 'none';
+            }
+    })
+})
