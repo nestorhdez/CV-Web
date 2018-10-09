6999b9b64bd4d207ccd23ecdefa79f6a48eeb2a7
unknown <jamiladbd18@gmail.com>
2018-10-09 17:25:23 +0100

    labe content missing

--- js/advanced-search.js
+++ js/advanced-search.js
@@ -1,14 +1,26 @@
-$('.btn').on("click", function () {
-    var shown = $('#input-name').val();
-    console.log(shown);
-    $('.area').append( '<span class="badge">' + shown + "</span> ");
-    shown = $('#city-option').val();
-    $('.area').append( '<span class="badge">' + shown + "</span> ");
-    shown = $('#experience').val();
-    $('.area').append( '<span class="badge">' + shown + "</span> ");
-    shown = $('input[name="language[]"]:checked').val();
-    $('.area').append( '<span class="badge">' + shown + "</span> ");
-    shown = $('input[name="skilss[]"]:checked').val();
-    $('.area').append( '<span class="badge">' + shown + "</span> ");
+$("#search-btn").on("click", function() {
+  var shown = $("#input-name").val();
+  $(".area").append('<span class="badge">' + shown + "</span> ");
+  shown = $("#city-option").val();
+  $(".area").append('<span class="badge">' + shown + "</span> ");
+  shown = $("#experience").val();
+  $(".area").append('<span class="badge">' + shown + "</span> ");
+  var check = $('input[name="language[]"]:checked').val();
+  console.log("valor check: " + check);
+  if (check == "on") {
+    $(".form-check-label").append('<span class="badge">' + (label)'.innerHtml'); //me faltael contenido del label//
+  } else if (check == "off") {
+    $(".form-check-label").hide();
+  }
+  $(".area").append('<span class="badge">' + check + "</span> ");
+  check = $('input[name="skilss[]"]:checked').val();
+  $(".area").append('<span class="badge">' + shown + "</span> ");
 });
 
+$("#adv-btn").on("click", function() {
+  $(".area").hide();
+});
+
+$("#search-btn").on("click", function() {
+  $(".area").show();
+});
