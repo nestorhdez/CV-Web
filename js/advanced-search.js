b934e20aadac81d5ae4cba9c6042aaf04e22aa06
unknown <jamiladbd18@gmail.com>
2018-10-09 16:37:18 +0100

    pull cause mistake

--- js/advanced-search.js
+++ js/advanced-search.js
@@ -2,13 +2,13 @@ $('.btn').on("click", function () {
     var shown = $('#input-name').val();
     console.log(shown);
     $('.area').append( '<span class="badge">' + shown + "</span> ");
-    var shown = $('#city-option').val();
+    shown = $('#city-option').val();
     $('.area').append( '<span class="badge">' + shown + "</span> ");
-    var shown = $('#experience').val();
+    shown = $('#experience').val();
     $('.area').append( '<span class="badge">' + shown + "</span> ");
-    var shown = $('input[name="language[]"]:checked').val();
+    shown = $('input[name="language[]"]:checked').val();
     $('.area').append( '<span class="badge">' + shown + "</span> ");
-    var shown = $('input[name="skilss[]"]:checked').val();
+    shown = $('input[name="skilss[]"]:checked').val();
     $('.area').append( '<span class="badge">' + shown + "</span> ");
 });
 
