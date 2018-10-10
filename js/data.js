f430a9287100d1c44bdbf8b74b6e3d7873836753
Fabián <frodriguez@20H.es>
2018-10-10 10:36:44 +0100

    Create listener event to create new user.

--- js/data.js
+++ js/data.js
@@ -1,15 +1,31 @@
-function takevalues() {
-  $("#registerSubmit").on("submit", function(event) {
+$("#registerSubmit").submit(function(event) {
     console.log("submit send...");
     event.preventDefault();
-    console.log($( this ));
-    let form = $ ( this )[0];
-    console.log(form.length);
-    for (i=0; i<form.length; i++){
-        console.log(form[i]);
-        console.log($(".form-control").value);
-    }
-    });
-}
 
-takevalues();
+    let name = $("#validationname").val();
+    let lastname = $("#validationlastname").val();
+    let phone = $("#InputPhone").val();
+    let zip = $("#validationZip").val();
+    let email = $("#InputEmail").val();
+    let username = $("#validationUsername").val();
+    let city = $("#validationCity").val();
+    let street = $("#validationStreet").val();
+    let state = $("#validationState").val();
+    let explab = $("#experience").val();
+
+    let languages = $('#languages input[type="checkbox"]:checked').map(function() {
+        return $(this).next("label").text().split(' ').join('%20');
+    }).get();
+
+    let skills = $('#skills input[type="checkbox"]:checked').map(function() {
+        return $(this).next("label").text().split(' ').join('%20');
+    }).get();
+
+    let repository = $("#validationRepository").val();
+
+    console.log(name, lastname, phone, zip, email, username, city, street, state, explab, languages, skills, repository);
+
+    return NewUser = [name, lastname, phone, zip, email, username, city, street, state, explab, languages, skills, repository];
+    });
+
+console.log(NewUser);
