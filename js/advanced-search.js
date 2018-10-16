1325ae547f98dd6c845bfcd5bbca60423f9d483c
unknown <jamiladbd18@gmail.com>
2018-10-16 19:38:58 +0100

    show adv

--- js/advanced-search.js
+++ js/advanced-search.js
@@ -1,25 +1,47 @@
 $("#search-btn").on("click", function() {
-  var shown = $("#input-name").val();
-  $(".area").append('<span class="badge">' + shown + "</span> ");
-  shown = $("#city-option").val();
-  $(".area").append('<span class="badge">' + shown + "</span> ");
-  shown = $("#experience").val();
-  $(".area").append('<span class="badge">' + shown + "</span> ");
-
-  console.log($("input[type=checkbox]"));
-  console.log($("input[type=checkbox]:checked").length);
-  var checked = $("input[type=checkbox]:checked");
-  console.log(checked[0].value);
-  console.log($("input[type=checkbox]:checked").next("label").text());
-  // convertir todo lo de abajo en un for o for.each
-for (i = 0; i < 8;){
-  if ($("input[type=checkbox]").is(':checked')) {
-    var check = $(".form-check-label").html();
-    $(".area").append('<span class="badge">' + check + "</span");
+  // for  text inputs
+  var name = $("#validationname").val();
+  $(".area").append('<span class="badge">' + name + "</span> ");
+  var lastname = $("#validationlastname").val();
+  $(".area").append('<span class="badge">' + lastname + "</span> ");
+  var city = $("#validationCity").val();
+  $(".area").append('<span class="badge">' + city + "</span> ");
+  var street = $("#validationStreet").val();
+  $(".area").append('<span class="badge">' + street + "</span> ");
+  var state = $("#validationState").val();
+  $(".area").append('<span class="badge">' + state + "</span> ");
+  var zip = $("#validationZip").val();
+  $(".area").append('<span class="badge">' + zip + "</span> ");
+  var experience = $("#experience").val() + " years";
+  $(".area").append('<span class="badge">' + experience + "</span> ");
+  // for checkeable inputs
+  var selectLanguage = $("#languages");
+  var languages = selectLanguage.find("input[type=checkbox]");
+  var selectSkill = $("#skills");
+  var skills = selectSkill.find("input[type=checkbox]");
+  
+  for (input of languages) {
+    // coge los inputs dentro del array language
+    if ($(input).prop("checked") === true) {
+      $(".area").append(
+        '<span class="badge">' + $(input).prop("name") + "</span"
+      );
+    }
   }
-}
 
 
+  // console.log(selectLanguage.find('input[type=checkbox]'))
+  // cada input ejecuta el siguiente código
+
+
+
+  for (input of skills) {
+    if ($(input).prop("checked") === true) {
+      $(".area").append(
+        '<span class="badge">' + $(input).prop("name") + "</span"
+      );
+    }
+  }
 });
 
 $("#adv-btn").on("click", function() {
@@ -29,25 +51,3 @@ $("#adv-btn").on("click", function() {
 $("#search-btn").on("click", function() {
   $(".area").show();
 });
-
-
-  // if ($("input[type=checkbox]").is(':checked')) {
-  //   var check = $("#lang-3").html();
-  //   $(".area").append('<span class="badge">' + check + "</span");
-  // }
-  // if ($("input[type=checkbox]").is(':checked')) {
-  //   var checked = $("#html-check").html();
-  //   $(".area").append('<span class="badge">' + check + "</span");
-  // }
-  // if ($("input[type=checkbox]").is(':checked')) {
-  //   var checked = $("#html-check").html();
-  //   $(".area").append('<span class="badge">' + check + "</span");
-  // }
-  // if ($("input[type=checkbox]").is(':checked')) {
-  //   var checked = $("#html-check").html();
-  //   $(".area").append('<span class="badge">' + check + "</span");
-  // }
-  // if ($("input[type=checkbox]").is(':checked')) {
-  //   var checked = $("#html-check").html();
-  //   $(".area").append('<span class="badge">' + check + "</span");
-  // }
