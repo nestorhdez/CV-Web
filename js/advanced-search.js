8a1a8bfedb21f6da99118fe5c1b9da201fc8b98c
unknown <jamiladbd18@gmail.com>
2018-10-17 13:34:37 +0100

    changed the way to display the variables and the for loop

--- js/advanced-search.js
+++ js/advanced-search.js
@@ -1,44 +1,46 @@
 $("#search-btn").on("click", function() {
   // for  text inputs
   var name = $("#validationname").val();
-  $(".area").append('<span class="badge">' + name + "</span> ");
-  var lastname = $("#validationlastname").val();
-  $(".area").append('<span class="badge">' + lastname + "</span> ");
   var city = $("#validationCity").val();
-  $(".area").append('<span class="badge">' + city + "</span> ");
+  var lastname = $("#validationlastname").val();
   var street = $("#validationStreet").val();
-  $(".area").append('<span class="badge">' + street + "</span> ");
   var state = $("#validationState").val();
-  $(".area").append('<span class="badge">' + state + "</span> ");
   var zip = $("#validationZip").val();
-  $(".area").append('<span class="badge">' + zip + "</span> ");
   var experience = $("#experience").val() + " years";
-  $(".area").append('<span class="badge">' + experience + "</span> ");
-  // for checkeable inputs
+  var spans = '<span class="badge badge-info">';
+  var finspans = '</span>';
   var selectLanguage = $("#languages");
   var languages = selectLanguage.find("input[type=checkbox]");
   var selectSkill = $("#skills");
   var skills = selectSkill.find("input[type=checkbox]");
+  var variables = [name, lastname, city, street, state, zip, experience];
+
+  for (let i = 0; i < variables.length; i++) {
+    $(".area").append( spans + variables[i] + finspans);
+  }
+  
+  // $(".area").append( spans + name + "</span> ");
+  // $(".area").append('<span class="badge badge-info">' + lastname + "</span> ");
+  // $(".area").append('<span class="badge">' + city + "</span> ");
+  // $(".area").append('<span class="badge">' + street + "</span> ");
+  // $(".area").append('<span class="badge">' + state + "</span> ");
+  // $(".area").append('<span class="badge">' + zip + "</span> ");
+  // $(".area").append('<span class="badge">' + experience + "</span> ");
+  // for checkeable inputs
+
   
   for (input of languages) {
     // coge los inputs dentro del array language
     if ($(input).prop("checked") === true) {
       $(".area").append(
-        '<span class="badge">' + $(input).prop("name") + "</span"
+        '<span class="badge badge-info">' + $(input).prop("name") + "</span>"
       );
     }
   }
-
-
-  // console.log(selectLanguage.find('input[type=checkbox]'))
-  // cada input ejecuta el siguiente código
-
-
-
   for (input of skills) {
     if ($(input).prop("checked") === true) {
       $(".area").append(
-        '<span class="badge">' + $(input).prop("name") + "</span"
+        '<span class="badge badge-info">' + $(input).prop("name") + "</span>"
       );
     }
   }
