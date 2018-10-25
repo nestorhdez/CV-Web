0a9aa0b967cc89348c7997df1a605ef61858cdce
unknown <jamiladbd18@gmail.com>
2018-10-25 20:42:43 +0100

    push for néstor and fabian

--- js/advanced-search.js
+++ js/advanced-search.js
@@ -1,50 +1,43 @@
 $("#search-btn").on("click", function() {
   // for  text inputs
+  $(".area").empty();
   var name = $("#validationname").val();
   var city = $("#validationCity").val();
   var lastname = $("#validationlastname").val();
   var street = $("#validationStreet").val();
   var state = $("#validationState").val();
   var zip = $("#validationZip").val();
-  var experience = $("#experience").val() + " years";
-  var spans = '<span class="badge badge-info">';
+  var experience = $("#experience").val();
+  var spans = '<span id="badge-font" class="badge badge-info mr-2">';
+  var deletion = '<button class="bg-transparent border-0" id="deletion"><i class="fas fa-times-circle"></i></button>';
   var finspans = '</span>';
   var selectLanguage = $("#languages");
   var languages = selectLanguage.find("input[type=checkbox]");
   var selectSkill = $("#skills");
   var skills = selectSkill.find("input[type=checkbox]");
   var variables = [name, lastname, city, street, state, zip, experience];
-
+  console.log("longvariables: " + variables.length + name + "name");
   for (let i = 0; i < variables.length; i++) {
-    $(".area").append( spans + variables[i] + finspans);
-  }
-  
-  // $(".area").append( spans + name + "</span> ");
-  // $(".area").append('<span class="badge badge-info">' + lastname + "</span> ");
-  // $(".area").append('<span class="badge">' + city + "</span> ");
-  // $(".area").append('<span class="badge">' + street + "</span> ");
-  // $(".area").append('<span class="badge">' + state + "</span> ");
-  // $(".area").append('<span class="badge">' + zip + "</span> ");
-  // $(".area").append('<span class="badge">' + experience + "</span> ");
-  // for checkeable inputs
+    if (variables[i] != 0) {
+    $(".area").append(spans + variables[i] + deletion + finspans);
+    if (i == 7) {
+      $(".area").append(spans + variables[i] + "years" + deletion + finspans);
+    } 
+  }}
 
-  
   for (input of languages) {
     // coge los inputs dentro del array language
     if ($(input).prop("checked") === true) {
-      $(".area").append(
-        '<span class="badge badge-info">' + $(input).prop("name") + "</span>"
-      );
+      $(".area").append(spans + $(input).prop("name") + deletion + finspans);
     }
   }
   for (input of skills) {
     if ($(input).prop("checked") === true) {
-      $(".area").append(
-        '<span class="badge badge-info">' + $(input).prop("name") + "</span>"
-      );
+      $(".area").append(spans + $(input).prop("name") + deletion + finspans);
     }
   }
 });
+
 
 $("#adv-btn").on("click", function() {
   $(".area").hide();
