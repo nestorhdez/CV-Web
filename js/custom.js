8c84ad0af8d03c6e13f007d8dc2cf52012d17527
nestorhdez <nestorhdez.26@gmail.com>
2018-10-26 19:34:16 +0100

    Integration of infinite scroll to the render of users.

--- js/custom.js
+++ js/custom.js
@@ -1,102 +1,31 @@
-const container = document.querySelector("#card-container");
-const object = new ListUsers;
-const callback = object.getAllUsers(object.url, object.filterName);
-
-function Scrollinfinite(container, object, callback){
-  this.container = container;
-  this.object = object;
+//Object infinite scroll
+function Scrollinfinite(callback){
+  
   this.callback = callback;
-  currentpage = 0;
+  this.currentpage = 1;
   let hasLoaded = false;
 
-  container.addEventListener("scroll", function(callback) {
-    console.log("Listening...");
-  
-    // Determine if an element has been totally scrolled
+  this.control = function (){
+    
+    const container = document.querySelector("#card-container");
+    currentpage = 0;
+
     if (container.scrollHeight - container.scrollTop === container.clientHeight && !hasLoaded) {
       console.log("Llegó al final.");
-      callback; // <= Invocar el generador de tarjetas por lotes.
-      createBoxes(10);
       hasLoaded = false;
+      this.callback(this.currentpage);
       console.log("Creado los 10 elementos.");
-      currentpage += 1;
-      console.log(currentpage);
+      this.currentpage += 1;
       return currentpage;
     }
-});
-}
 
-Scrollinfinite(container, object, callback);
+  }.bind(this);
 
+  this.initScroll = function () {
+    
+    const container = document.querySelector("#card-container");
+    container.addEventListener("scroll", this.control);
 
-/**
- * @name sleep function to simulation waiting data.
- * @param {*} milliseconds
- */
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if (new Date().getTime() - start > milliseconds) {
-      break;
-    }
   }
+
 }
-
-/**
- *  @name card Constructor of one element.
- */
-function card() { // <== createCard();
-  let card = `
-  <div class="card mr-3 mb-3 card-user shadow list-group-item-action" style="width: 18rem;">
-    <div class="card-body text-center">
-      <div class="d-flex flex-column justify-content-center mb-3">
-        <div class="d-flex mx-auto profile-picture mb-1">
-          <img class="img-user rounded-circle" src="../img/default-profile-picture.jpg">
-        </div>
-        <h5 class="card-title d-inline user-name">name</h5>
-      </div>
-      <div class="text-left">
-      <h6 class="card-subtitle mb-3 text-center">Contact information</h6>
-      <p class="m-0 city-user"><b>City: </b>address.city</p>
-        <p class="m-0"><b>Email: </b><a href="mailto:email">email</a></p>
-        <p><b>Phone: </b>phone</p>
-
-        <div class="d-flex justify-content-between">
-            <button type="button" class="btn btn-info btn-sm">Edit</button>
-            <button type="button" class="btn btn-info btn-sm" id="fulldata" data-toggle="modal" data-target="#ModalCenter">Detail</button>
-            <button type="button" class="btn btn-primary btn-sm">Delete</button>
-        </div>                 
-      </div>
-    </div>
-  </div>`;
-  container.innerHTML += card;
-}
-/**
- * @name createBoxes Function create "n" elements.
- * @param {number} num
- */
-function createBoxes(num) {
-  let hasLoaded = false;  // <= Mejorar el creador de tarjetas para poder renderizar por lote.
-  const loader = document.querySelector("#loader");
-  loader.classList.toggle("hide");
-  console.log("Esperando a cargar...");
-  sleep(1000);
-  for (let i = 0; i < num; i++) {
-    card();
-  }
-  console.log("Has llegado al final del contenido");
-  hasLoaded = true;
-  loader.classList.toggle("hide");
-}
-
-// TESTING //
-
-// if ($(document).height() - win.height() === win.scrollTop()) {
-/**
- * @event scroll Waiting for scrolling bottom of container.
- */
-
-
-// Definir en el objeto, el parametro de contenedor, callback, el objeto que lo usa)
-
-console.log("This running.");
--- js/render.js
+++ js/render.js
@@ -1,28 +1,28 @@
-
 //Object List of Users
 function ListUsers() {
 
-    this.url = "https://jsonplaceholder.typicode.com/users";
+    this.url = "https://cv-mobile-api.herokuapp.com/api/users";
 
     //Call the ajax and get the list of users
-    this.getAllUsers = function ( url, callback, val ) {
+    this.getAllUsers = function ( callback ) {
 
-        $.ajax ({
-            url: url,
+         $.ajax ({   
+            url: this.url,
             dataType: "json"
         }).done( function(data) {
-
+            
             //this control do optional the callback
             if(typeof callback == "function" ){
-                callback( data, val );
+                callback( data );
                 console.log("Callback parameter is a function");
             }else{
                 console.log("Callback parameter isn't a function");
             }
 
+            return data;
         });
-
     };
+    
 
     this.renderUsers = function ( arr ) {
         console.log(arr);
@@ -36,19 +36,19 @@ function ListUsers() {
                     <div class="card-body text-center">
                         <div class="d-flex flex-column justify-content-center mb-3">
                             <div class="d-flex mx-auto profile-picture mb-1">
-                                <img class="img-user rounded-circle" src="../img/default-profile-picture.jpg">
+                                <img class="img-user rounded-circle" src="${val.profilePicture}">
                             </div>
                             <h5 class="card-title d-inline user-name">${val.name}</h5>
                         </div>
                         <div class="text-left">
                             <h6 class="card-subtitle mb-3 text-center">Contact information</h6>
-                            <p class="m-0 city-user"><b>City: </b>${val.address.city}</p>
+                            <p class="m-0 city-user"><b>City: </b>${val.location.city}</p>
                             <p class="m-0"><b>Email: </b><a href="mailto:${val.email}">${val.email}</a></p>
-                            <p><b>Phone: </b>${val.phone}</p>
+                            <p><b>Website: </b>${val.website}</p>
 
                             <div class="d-flex justify-content-between align-items-end">
                                 <button type="button" class="btn btn-info btn-sm">Edit</button>
-                                <button type="button" class="btn btn-modal btn-info btn-sm" id="${val.id}" data-toggle="modal" data-target="#ModalCenter">Detail</button>
+                                <button type="button" class="btn btn-modal btn-info btn-sm" id="${val._id}" data-toggle="modal" data-target="#ModalCenter">Detail</button>
                                 <button type="button" class="btn btn-primary btn-sm">Delete</button>
                             </div>
                         </div>
@@ -69,43 +69,68 @@ function ListUsers() {
     //It's indise of renderUsers because it only has to work with the cards that are already rendered.
     this.renderModal = function(arr) {
         $('.btn-modal').click(function(e){
-            console.log('click done');
-            console.log(arr[e.target.id - 1]);
-            console.log('User id ' + e.target.id);
-            let user = arr[e.target.id - 1];
-            $('#ModalCenterTitle').empty().html(user.name);
+            // console.log('click done');
+            // console.log('User id ' + e.target.id);
+            arr.forEach( function(val){
+                if(val._id == e.target.id){
+                    let user = val;
 
-            $('#city').empty().html(user.address.city);
+                    $('#ModalCenterTitle').empty().html(user.name);
 
-            $('#street').empty().html(user.address.street);
-            
-            $('#zipcode').empty().html(user.address.zipcode);
+                    $('#city').empty().html(user.location.city);
 
-            $('#email').empty().html(user.email);
+                    $('#street').empty().html(user.location.street);
+                    
+                    $('#zipcode').empty().html(user.location.zipcode);
 
-            $('#phone').empty().html(user.phone);
-            
-            $('#website').empty().html(user.website);
+                    $('#email').empty().html(user.email);
 
-            $('#company').empty().html(user.company.name);
+                    $('#phone').empty().html(user.phone);
+                    
+                    $('#website').empty().html(user.website);
+
+                    $('#company').empty().html(user.company.name);
+                }
+            });
 
         })
     }
 
+    //Return an array that represent a page of the inital array.
+    //As argument you should pass it the initial array, how many
+    // elementes do you need per page and the page nº that you need.
+    this.pagination = function (arr, perpage, page) {     
+        return arr.slice(perpage*(page-1), perpage*page);
+    }
+
     //The callback parameter has to be passed on the getAllUsers function
-    this.filterName = function ( arr, callback ){
+    this.filterName = function ( currentPage ){
 
-        let nameInput = document.querySelector("#input-name").value.toLowerCase();
 
-        let filterName = arr.filter(user => user.name.toLowerCase().indexOf(nameInput) != -1);
-        console.log(filterName);
-        
-        if(filterName.length > 0 ){
-            $( "#card-container" ).empty();
-            this.renderUsers( filterName );
-        }else{
-            $( "#card-container" ).empty().html("<h1>There are not any coincidence</h1>");
-        }
+        let promise = new Promise((resolve, reject) => {
+
+            this.getAllUsers( resolve );
+
+        }).then((allUsers) => {
+
+            console.log(allUsers);
+
+            let nameInput = document.querySelector("#input-name").value.toLowerCase();
+            
+            let filterByName = allUsers.filter(user => user.name.toLowerCase().indexOf(nameInput) != -1);
+
+            console.log( filterByName );  
+
+            if(filterByName.length < 10 ) {
+                $( "#card-container" ).empty();
+                this.renderUsers( this.pagination(filterByName, 10, 0) );
+                console.log("less than 10 users");
+            }else {
+                
+                this.renderUsers( this.pagination(filterByName, 10, currentPage) );
+                console.log(this.pagination(filterByName, 10, currentPage));
+            }
+        })
         
     }.bind(this);
 
@@ -113,10 +138,11 @@ function ListUsers() {
 
 //Calling the FilterUsers and createCards functions on form submit
 let list = new ListUsers;
+let scroll = new Scrollinfinite(list.filterName).initScroll();
 
 $( "#adv-search-form" ).on( "submit", function(e) {
     //Don't refresh the page when submit
     e.preventDefault();
 
-    list.getAllUsers(list.url, list.filterName);
+    list.filterName(1);
 })
