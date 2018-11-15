f0fe0b17ac297948fc0ae7ea7174ec7ebfbebe1e
nestorhdez <nestorhdez.26@gmail.com>
2018-11-15 11:13:48 +0000

    Creating checkbox dynamically.

--- js/render.js
+++ js/render.js
@@ -13,7 +13,6 @@ function ListUsers() {
             
             if(typeof callback == "function" ){
                 callback( data );
-                console.log("Callback parameter is a function");
             }else{
                 console.log("Callback parameter isn't a function");
             }
@@ -24,7 +23,6 @@ function ListUsers() {
     
 
     this.renderUsers = function ( arr ) {
-        // console.log(arr);
   
         //Create a card for each user
         arr.forEach(function(val) {
@@ -34,7 +32,8 @@ function ListUsers() {
                 <div class="card-body text-center">
                 <div class="row mb-4">        
                     <div class="d-flex justify-content-between align-self-end mt-3 mx-auto">
-                        <button type="button" class="btn btn-info btn-sm mx-1">Edit</button>
+                        <button type="button" class="btn-edit btn btn-info btn-sm mx-1" id="${val._id}" data-toggle="modal"
+                        data-target="#ModalCenter">Edit</button>
                         <button type="button" class="btn btn-info btn-modal btn-sm mx-1" id="${val._id}" data-toggle="modal"
                             data-target="#ModalCenter">Detail</button>
                         <button type="button" class="btn btn-cobalt btn-sm mx-1">Delete</button>
@@ -43,7 +42,7 @@ function ListUsers() {
                     <div class="row">
                         <div class="d-flex flex-column mx-auto justify-content-center mb-3">
                             <div class="d-flex mx-auto profile-picture mb-1">
-                                <img class="img-user rounded-circle" src="${val.profilePicture}">
+                                <img class="img-user rounded-circle" src="${val.avatar}">
                             </div>
                             <h5 class="card-title d-inline user-name text-capitalize">${val.name}</h5>
                         </div>
@@ -51,9 +50,9 @@ function ListUsers() {
                     <div class="row px-3">    
                         <div class="d-flex flex-column flex-nowrap text-left my-2">
                             <h6 class="card-subtitle mb-3 text-center">Contact information</h6>
-                            <p class="m-0 text-capitalize"><strong>City: </strong>${val.location.city}</p>
-                            <p class="m-0 text-capitalize"><strong>Country: </strong>${val.location.country}</p>
-                            <p class="m-0 text-capitalize"><strong>State: </strong>${val.location.state}</p>
+                            <p class="m-0 text-capitalize"><strong>City: </strong>${val.address.city}</p>
+                            <p class="m-0 text-capitalize"><strong>Country: </strong>${val.address.country}</p>
+                            <p class="m-0 text-capitalize"><strong>State: </strong>${val.address.state}</p>
                             <p class="m-0 font-italic text-capitalize"><strong>Skills: </strong>${val.skills.join(', ')}</p>
                             <p class="m-0"><strong>Email: </strong><a href="mailto:${val.email}">${val.email}</a></p>
                         </div>
@@ -73,8 +72,8 @@ function ListUsers() {
     /*Change the data of the modal when click on a user card.*/
     this.renderModal = function(arr) {
         $('.btn-modal').click(function(e){
-            console.log('click done');
-            console.log('User id ' + e.target.id);
+            // console.log('click done');
+            // console.log('User id ' + e.target.id);
             arr.forEach( function(val){
                 // console.log("val_id: ", val._id);
                 // console.log("e.id: ", e.target.id);
@@ -85,11 +84,11 @@ function ListUsers() {
                     
                     $('#ModalCenterTitle').empty().html(user.name);
 
-                    $('#city').empty().html(user.location.city);
+                    $('#city').empty().html(user.address.city);
 
-                    $('#country').empty().html(user.location.country);
+                    $('#country').empty().html(user.address.country);
                     
-                    $('#state').empty().html(user.location.state);
+                    $('#street').empty().html(user.address.street);
 
                     $('#language').empty().html(user.languages.join(', '));
 
@@ -116,7 +115,6 @@ function ListUsers() {
         return arr.slice(perpage*(page-1), perpage*page);
     }
 
-
     this.filterUsers = function ( currentPage ){
 
         new Promise((resolve, reject) => {
@@ -125,7 +123,6 @@ function ListUsers() {
 
         }).then((allUsers) => {
 
-            console.log(allUsers);
             // Inputs
             let nameInput = document.querySelector("#input-name").value.toLowerCase();
             let usernameInput = document.querySelector("#validationusername").value.toLowerCase();
@@ -136,7 +133,7 @@ function ListUsers() {
             let stateInput = document.querySelector("#validationState").value.toLowerCase();
             let companyInput = document.querySelector("#validationcompany").value.toLowerCase();
             let jobTitleInput = document.querySelector("#validationjob").value.toLowerCase();
-            let experienceSelect = document.querySelector("#experience").value.toLowerCase();
+            let experienceSelect = document.querySelector("#experience-search").value.toLowerCase();
             //CheckBoxes
             let languages = document.querySelectorAll('#languages .form-check-input');
             let checkedLanguages = [...languages].filter(lang => lang.checked == true ).map(lang => lang.defaultValue.toLowerCase());
@@ -232,9 +229,7 @@ function ListUsers() {
                 });
                 filterBySkills.forEach(removeFilteredUser);
             }
-            
-            console.log( allFilters );
-            // console.log( allFilters[0].gender );
+
 
             if( allFilters.length === 0 ){
                 $( "#card-container" ).empty();
@@ -251,14 +246,110 @@ function ListUsers() {
             }
 
             this.renderModal(allUsers);
+            this.editUsers(allUsers);
 
         });
         
     }.bind(this);//Bind ListUsers object
 
+    this.getUserValue = function (val, callback){
+        fetch(`https://cv-mobile-api.herokuapp.com/api/${val}`)
+        .then(res => res.json())
+        .then(data => {
+            callback(data);
+        });
+    }
+
+    this.createCheckBoxes = function ( arr ){
+        let checkBoxArr = [];
+        arr.forEach(val => {
+            let checkBox =
+            `<div class="form-check">
+                <input class="form-check-input" type="checkbox" id="${val.label}-check" name="${val.type}"
+                    value="${val._id}">
+                <label class="form-check-label mr-5" for="${val.label}-check">${val.label}</label>
+            </div>`;
+            checkBoxArr.push(checkBox);
+        })
+        return checkBoxArr;
+    }
+
+    this.renderCheckBoxArr = function (container, val){
+        
+        new Promise ( (resolve, reject) => {
+            this.getUserValue(val, resolve);
+        }).then((val) => {
+            let checkbox = this.createCheckBoxes( val );
+            checkbox.forEach(val =>{
+                $(container).append(val);
+            });
+        });
+    }.bind(this);
+
+    this.createFormEditUser = function(user) {
+
+        $('#ModalCenterTitle').empty().html(user.name);
+        $('#profilePicture').attr("src", user.avatar);
+        $('.modal-subtitle').empty().html('Edit information');
+
+        for (let key in user){
+            // console.log(key);
+            switch (key) {
+                case '_id':
+                case 'registeredDate':
+                case 'profilePicture':
+                case '__v':
+                    null
+                    break;
+                case 'skills':
+                    $('#skills-modal').empty();
+                    this.renderCheckBoxArr('#skills-modal', 'skills');
+                    break;
+                case 'languages':
+                    $('#language').empty();
+                    this.renderCheckBoxArr('#language', 'langs');
+                    break;
+                    
+                case 'address':
+                    for (let val in user[key]) {
+                        let addressKey = user[key];
+                        $(`#${val}`).empty().html(`<input style="display: block" value="" name="${val}" placeholder="${addressKey[val]}"></input>`)
+                    }
+                    break;
+                
+                default:
+                $(`#${key}`).empty().html(`<input style="display: block" value="" name="${key}" placeholder="${user[key]}"></input>`);
+                    break;
+            }
+        }
+
+        $('.card-body-text').has('.btn-edit-container').length > 0 ? null :
+            $('.card-body-text').append( 
+                `<div class="btn-edit-container">
+                    <button type="submit" class="btn btn-sm btn-info" id="edit-user-btn">Search</button>
+                </div>`
+        ); 
+    }.bind(this);
+
+    this.editUsers = function (){
+        $('.btn-edit').click(function(e){
+            console.log('click done');
+            console.log('User id ' + e.target.id);
+            
+            $.ajax(`https://cv-mobile-api.herokuapp.com/api/users/${e.target.id}`)
+            .done( user => {
+                console.log(user);
+                this.createFormEditUser(user); 
+            })
+        }.bind(this));
+    }.bind(this);
+
+
 }
 
 let list = new ListUsers;
+list.renderCheckBoxArr('#languages-search', 'langs');
+list.renderCheckBoxArr('#skills', 'skills');
 let scroll = new Scrollinfinite(list.filterUsers).initScroll();
 
 //Calling the FilterUsers functions and render users on form's submit
