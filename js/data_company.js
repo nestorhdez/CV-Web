2321d2bf6ead9c130dfd08f1444350e75ba2eeda
Fabian <frodriguez@20h.es>
2018-11-25 01:47:04 +0000

    Add css theme.min, redisign form, working to js to take values and send.

--- js/data_company.js
+++ js/data_company.js
@@ -11,7 +11,6 @@ function NewCompany(
     website,
     logo,
     bio,
-    jobOffers, // Array? []
     employes, // Numbers
     socialUrls) { // Array de objectos
     this.name = name;
@@ -26,8 +25,50 @@ function NewCompany(
     this.website = website;
     this.logo = logo;
     this.bio = bio;
-    this.jobOffers = jobOffers; // Objeto?
+    this.jobOffers = []; // Objeto?
     this.employes = employes; // Numbers
     this.socialUrls = socialUrls; // Array de objectos
   }
 
+
+$("#registerCompanySubmit").submit(function(e) {
+  console.log("sumit actived.");
+  e.preventDefault();
+
+  // Need check CIF-NIF
+
+  let name = $("#validationCompname").val();
+  let phone = $("#InputPhone").val();
+  let email = $("#validationInputEmail").val();
+  let docType = $("#SelectDocType").val(); // CIF-NIF
+  let docNumber = $("#docNumber").val();// String
+  let zip = $("#validationZip").val();
+  let street = $("#validationStreet").val();
+  let city = $("#validationCity").val();
+  let country = $("#validationCountry").val();
+  let website = $("validationWebsite").val();
+  let logo = "";
+  let bio = $("#validationBio").text();
+  let jobOffers = []; // Objeto?
+  let employes = $("#validationemployees").val(); // Numbers
+  let socialUrls = []
+
+  console.log(
+    name,
+    phone,
+    email,
+    docType,
+    docNumber,
+    zip,
+    street,
+    city,
+    country,
+    website,
+    logo,
+    bio,
+    jobOffers,
+    employes,
+    socialUrls,
+  );
+
+})
