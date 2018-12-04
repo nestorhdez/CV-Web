ae45aab2b417a894f2d5a53a7ba781999fab7441
Fabián <frodriguez@20H.es>
2018-12-04 09:55:36 +0000

    Little fix in render profile card.

--- js/render.js
+++ js/render.js
@@ -44,7 +44,7 @@ class Users extends Model{
                         <div class="d-flex flex-column flex-nowrap text-left my-2">
                             ${user.address.city ? '<p class="m-0 text-capitalize"><strong>City: </strong>' + user.address.city + '</p>' : ''}
                             ${user.address.country ? '<p class="m-0 text-capitalize"><strong>Country: </strong>' + user.address.country + '</p>' : ''}
-                            ${user.address.state ? '<p class="m-0 text-capitalize"><strong>State: </strong>' + user.address.state + '</p>' : ''}
+                            ${user.address.street ? '<p class="m-0 text-capitalize"><strong>Street: </strong>' + user.address.street + '</p>' : ''}
                             ${skills.length > 0 ? '<p class="m-0 font-italic text-capitalize"><strong>Skills: </strong>' + skills.join(', ') + '</p>' : ''}
                             ${langs.length > 0 ? '<p class="m-0 font-italic text-capitalize"><strong>Languages: </strong>' + langs.join(', ') + '</p>' : ''}
                             <p class="m-0"><strong>Email: </strong><a href="mailto:${user.email}">${user.email}</a></p>
